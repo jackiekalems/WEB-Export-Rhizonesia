573ce8bc52553fe0f975ceadbe9839ca37e7d098
Fajarshodiq1 <fajarshodiq1220@gmail.com>
2024-06-04 17:52:45 +0700

    update hero and footer

---
+++ resources/views/pages/footer.blade.php
@@ -0,0 +1,92 @@
+<footer class="bg-gray-900 text-center lg:text-left mt-5">
+    <div class="mx-6 py-10 text-center md:text-left">
+        <div class="flex flex-col lg:flex-row justify-between gap-8 container mx-auto">
+            <!-- TW Elements section -->
+            <div class="">
+                <h6 class="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start text-black">
+                    <svg class="h-10 mr-3" width="51" height="70" viewBox="0 0 51 70" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0)">
+                            <path d="M1 53H27.9022C40.6587 53 51 42.7025 51 30H24.0978C11.3412 30 1 40.2975 1 53Z"
+                                fill="#76A9FA"></path>
+                            <path
+                                d="M-0.876544 32.1644L-0.876544 66.411C11.9849 66.411 22.4111 55.9847 22.4111 43.1233L22.4111 8.87674C10.1196 8.98051 0.518714 19.5571 -0.876544 32.1644Z"
+                                fill="#A4CAFE"></path>
+                            <path d="M50 5H23.0978C10.3413 5 0 15.2975 0 28H26.9022C39.6588 28 50 17.7025 50 5Z"
+                                fill="#1C64F2"></path>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0">
+                                <rect width="51" height="70" fill="white"></rect>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                    <span class="self-center text-lg font-semibold text-white whitespace-nowrap">Rhizonesia</span>
+                </h6>
+                <div>
+                    <a href="https://maps.app.goo.gl/KKEXhWte43mySfnT6" class="text-white text-sm md:text-base"
+                        target="_blank">
+                        <i class="ri-home-3-line text-xl md:text-2xl text-white"></i>
+                        Jl. Raya Blora-Rembang No.km.1, Kabupaten Blora, Jawa Tengah
+                    </a>
+                </div>
+                <div>
+                    <a href="salutmustikablora.com" class="text-white text-sm md:text-base" target="_blank">
+                        <i class="ri-global-line text-xl md:text-2xl text-white"></i>
+                        rhizonesia.com
+                    </a>
+                </div>
+            </div>
+            <!-- Useful links section -->
+            <div class="text-white">
+                <h1 class=" mb-4 flex justify-center text-white font-semibold uppercase md:justify-start">
+                    Fakultas & Prodi
+                </h1>
+                <p class="mb-4 text-sm md:text-base">
+                    <a href="#stats">Fakultas Matematika dan IPA</a>
+                </p>
+                <p class="mb-4 text-sm md:text-base">
+                    <a href="#stats">Fakultas Ekonomi</a>
+                </p>
+                <p class="mb-4 text-sm md:text-base">
+                    <a href="#stats">Fakultas Ilmu Sosial dan Politik</a>
+                </p>
+                <p class="mb-4 text-sm md:text-base">
+                    <a href="#stats">Fakultas Keguruan dan Ilmu Pendidikan</a>
+                </p>
+            </div>
+            <!-- Contact section -->
+            <div>
+                <h6 class="mb-4 flex text-white justify-center font-semibold uppercase md:justify-start">
+                    Contact
+                </h6>
+                <p class="mb-4 text-white flex items-center justify-center md:justify-start ">
+                    <a href="whatsapp://send?text=Hello&phone=+6289513910161" target="_blank"
+                        class="text-sm md:text-base">
+                        <i class="ri-whatsapp-line mx-3 text-xl md:text-2xl"></i>
+                        +62 895-1391-0161
+                    </a>
+                </p>
+
+                <p class="mb-4 text-white flex items-center justify-center md:justify-start ">
+                    <a href="https://www.instagram.com/salutmustikablora?igsh=aWkzOXlxeDNldjF2" target="_blank"
+                        class="text-sm md:text-base">
+                        <i class="ri-instagram-line mx-3 text-xl md:text-2xl"></i>
+                        salutmustikablora
+                    </a>
+                </p>
+
+                <p class="mb-4 text-white flex items-center justify-center md:justify-start text-sm md:text-base">
+                    <i class="ri-phone-line mx-3 text-2xl"></i>
+                    0296-530 15 35
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!--Copyright section-->
+    <div class="bg-black/5 text-white p-6 text-center text-sm md:text-base">
+        <span>© 2024 Copyright:</span>
+        <a class="font-semibold" href="https://tw-elements.com/">Rizhonesia</a>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,54 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     @vite('resources/css/app.css')
+    <style>
+        .swiper-slide {
+            background-size: cover;
+            background-position: center;
+        }
+
+        .cloud-effect {
+            bottom: -1px;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        @media (max-width: 768px) {
+            .swiper-container {
+                height: 40%;
+            }
+        }
+
+        @media (min-width: 769px) {
+            .swiper-container {
+                height: 100%;
+            }
+        }
+    </style>
 </head>
 
 <body>
     @include('component.navbar')
+    @include('pages.hero')
+    @include('pages.footer')
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
 </body>
 
 </html>
